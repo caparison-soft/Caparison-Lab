8d4e791395ac43a12c9ce9ac2036319c01176acf
farhan Kabir <farhankabir@Farhans-MacBook-Air.local>
2026-05-10 15:50:40 +0600

    chore: add vercel.app redirect to custom domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'caparison-lab.vercel.app',
+          },
+        ],
+        destination: 'https://www.caparisonlab.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
